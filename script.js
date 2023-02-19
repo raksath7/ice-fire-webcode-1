e61bafa8d1839fd90e0a46d41940df00bb69bce8
raksath <raksath7@gmail.com>
2023-02-19 08:40:26 +0530

    search added

--- script.js
+++ script.js
@@ -1,91 +1,100 @@
+let main = document.createElement("main");
+main.setAttribute("class","main")
+document.body.append(main)
+
 let title = document.createElement("h1");
 title.innerHTML = "BOOK STORE";
 title.setAttribute("class", "title");
-document.body.append(title);
+main.append(title);
+
+let form = document.createElement("form");
+form.setAttribute("class","class")
+main.append(form);
+
+let label = document.createElement("label");
+label.innerText = "Search";
+label.setAttribute("for","input")
+form.append(label);
+
+let input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("id", "input");
+input.setAttribute("name", "input");
+form.append(input);
+
 
 let D = document.createElement("div");
 document.body.append(D);
 D.setAttribute("class", "App");
 
-// D1.setAttribute("class","D1")
+form.setAttribute("submit", handleSubmit);
+
+
 async function getData() {
   await fetch("https://anapioficeandfire.com/api/books")
     .then((response) => response.json())
-    .then((details) => {
-      // result && console.log(result[0]);
-      for (let i of details) {
-        console.log(details[i])
-      // result.map((details) => {
-        let D1 = document.createElement("div");
+    .then((res) => {
+      console.log(res);
+      res.map((details) => {
+        let D1 = document.createElement("ul");
         D1.setAttribute("class", "D1");
+        D1.setAttribute("id", "paginated-list");
+
         D.append(D1);
         //name
-        let name = document.createElement("div");
-        D1.append(name);
+        let name = document.createElement("li");
         name.setAttribute("class", "name");
-        // name.innerHTML = `Name: ${details.name}`;
-        let nameN = document.createElement("div");
-        nameN.innerHTML = `Book Name`;
-        let nameValue = document.createElement("div");
-        nameValue.innerHTML = `${details[i]["name"]}`;
-        name.append(nameN);
-        name.append(nameValue);
+        D1.append(name);
+        name.innerHTML = `Name: ${details.name}`;
+
         // isbn
-        let isbn = document.createElement("div");
+        let isbn = document.createElement("li");
         isbn.setAttribute("class", "isbn");
         D1.append(isbn);
-        let isbnN = document.createElement("div");
-        isbnN.innerHTML = `isbn`;
-        let isbnValue = document.createElement("div");
-        isbnValue.innerHTML = ` ${details.isbn}`;
-        isbn.append(isbnN);
-        isbn.append(isbnValue);
+         isbn.innerHTML = `isbn: ${details.isbn}`;
+        
         //pgno
-        let pgNo = document.createElement("div");
+        let pgNo = document.createElement("li");
         D1.append(pgNo);
         pgNo.setAttribute("class", "pgNo");
-        // pgNo.innerHTML = `Number Of Pages: ${details.numberOfPages}`;
-        let pgNoN = document.createElement("div");
-        pgNoN.innerHTML = `Number Of Pages:`;
-        let pgNoValue = document.createElement("div");
-        pgNoValue.innerHTML = ` ${details.numberOfPages}`;
-        pgNo.append(pgNoN);
-        pgNo.append(pgNoValue);
+        pgNo.innerHTML = `Number Of Pages: ${details.numberOfPages}`;
+
         //authors
-        let authors = document.createElement("div");
-        D1.append(authors);
+        let authors = document.createElement("li");
         authors.setAttribute("class", "authors");
-        let authorsN = document.createElement("div");
-        authorsN.innerHTML = `Author:`;
-        let authorsValue = document.createElement("div");
-        authorsValue.innerHTML = `  ${details.authors}`;
-        authors.append(authorsN);
-        authors.append(authorsValue);
+        D1.append(authors);
+         authors.innerHTML = `authors: ${details.authors}`;
+
         // publisher
-        let publisher = document.createElement("div");
+        let publisher = document.createElement("li");
         D1.append(publisher);
         publisher.setAttribute("class", "publisher");
-        // publisher.innerHTML = `Publisher: ${details.publisher}`;
-        let publisherN = document.createElement("div");
-        publisherN.innerHTML = `publisher:`;
-        let publisherValue = document.createElement("div");
-        publisherValue.innerHTML = `  ${details.publisher}`;
-        publisher.append(publisherN);
-        publisher.append(publisherValue);
-        //released
-        let released = document.createElement("div");
+        publisher.innerHTML = `Publisher: ${details.publisher}`;
+
+        let released = document.createElement("li");
         D1.append(released);
         released.setAttribute("class", "released");
-        // released.innerHTML = `Released: ${details.released}`;
-        let releasedN = document.createElement("div");
-        releasedN.innerHTML = `released:`;
-        let releasedValue = document.createElement("div");
-        releasedValue.innerHTML = `  ${details.released}`;
-        released.append(releasedN);
-        released.append(releasedValue);
-      };
-  
-    })
+        released.innerHTML = `Released: ${details.released}`;
+
+      })
+    }
+    ) 
     .catch((err) => console.log(err));
 }
-getData();
+
+let li = document.querySelectorAll("li");
+function handleSubmit(e) {
+  e.preventDefault();
+  // let value = e.target.value;
+  let value = document.querySelector("input").value;
+  for (var i = 0; i < li.length; i++) {
+        if(li[i].textContent.toLowerCase()
+                .includes(value.toLowerCase())) {
+            li[i].classList.add("show");
+        } else {
+            li[i].classList.remove("show");
+        }
+    }
+}
+
+getData()
